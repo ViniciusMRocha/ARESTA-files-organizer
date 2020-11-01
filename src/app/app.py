f2ecd445b71fa1c5d2bb2d00bf6088d53a6f391e
Vinicius Rocha <viniciusrocha@Vini-MacBook.local>
2020-11-01 11:37:20 -0800

    Order, counter and map were implemented

--- src/app/app.py
+++ src/app/app.py
@@ -1,10 +1,15 @@
+from typing import Counter
+from PyPDF2 import PdfFileReader
 from PyPDF2 import PdfFileReader, PdfFileWriter, PdfFileMerger
 from pdf2image import convert_from_path, convert_from_bytes
 from pdf2image.exceptions import (
     PDFInfoNotInstalledError, PDFPageCountError, PDFSyntaxError)
 import os
 import cv2
+import json
 import numpy as np
+import glob
+import time
 
 
 def convert_single_page(input_pdf, page, path):
@@ -44,12 +49,18 @@ def compare_images(flag_img_path, compared_img_path):
         return False
 
 
-def app(file_prefix, pdf_path):
+def load_map(path_to_map):
+    with open(path_to_map) as json_file:
+        json_content = json.load(json_file)
+        return json_content
+
+
+def app(file_prefix, pdf_path, counter=1, order='asc', map=None):
+    startTime = time.time()
     out_temp_path = '../out/temp'
     input_pdf = PdfFileReader(str(pdf_path))
     input_length = input_pdf.getNumPages()
     pdf_merger = PdfFileMerger()
-    output_counter = 0
 
     # Loop page by page
     for page in range(input_length):
@@ -76,12 +87,15 @@ def app(file_prefix, pdf_path):
             # True for when the page matches the flag, false otherwise
             if compare_result:
                 # Create new pdf with to save the pages that are not equal to the flag
-                with open('../out/{}-{}.pdf'.format(file_prefix, output_counter), "wb") as output_file:
+                with open('../out/{}-{}.pdf'.format(file_prefix, counter), "wb") as output_file:
                     pdf_merger.write(output_file)
                 # Update the output counter
-                # TODO: Set logic here for count for years, months, ids
-                output_counter += 1
-                # Create a Merger object that will combine the files
+                if order == 'asc':
+                    counter += 1
+                elif order == 'desc':
+                    counter -= 1
+
+                    # Create a Merger object that will combine the files
                 pdf_merger = PdfFileMerger()
             else:
                 # Append page to the correspondent pdf
@@ -95,14 +109,52 @@ def app(file_prefix, pdf_path):
     os.remove('{}/flag.pdf'.format(out_temp_path))
     os.remove('{}/flag.jpeg'.format(out_temp_path))
 
+    # Rename files if map is present
+    if map is not None:
+        # Load the map
+        map_dict = load_map(map)
+        # Get the path to the files to be renamed
+        file_list = glob.glob('../out/{}*.pdf'.format(file_prefix))
+        # Iterate over each file
+        for count, file_path in enumerate(file_list, 1):
+            # Get the value for each key
+            map_value = map_dict["{}".format(count)]
+            # Set up new variable name
+            new_file_name = '../out/{}-{}-{}.pdf'.format(
+                file_prefix, count, map_value)
+            # Rename the original files
+            os.rename(file_path, new_file_name)
 
-# Running APP
-file_prefix = 'FileName'
-pdf_path = '../data/test-input.pdf'
+    print("\n======= Elapsed Time ==================================")
+    endTime = time.time() - startTime
+    print(time.strftime("%H:%M:%S", time.gmtime(endTime)))
 
-app(file_prefix, pdf_path)
+
+if __name__ == "__main__":
+    # Running APP - Test Large File
+    file_prefix = 'Green_Card'
+    pdf_path = '../data/Application_Test.pdf'  # 400+ pages
+    # app(file_prefix, pdf_path)
+
+    # Running APP - Testing Map
+    file_prefix = 'Test_File'
+    pdf_path = '../data/test-input.pdf'
+    map = '../data/map.json'
+    app(file_prefix, pdf_path, map=map)
+
+    # Running APP - Test Order desc
+    file_prefix = 'Test_Order_Desc'
+    pdf_path = '../data/test-input.pdf'
+    counter = 2020
+    order = 'desc'
+    map = '../data/map.json'
+    app(file_prefix, pdf_path, counter, order)
+
+    # Running APP - Test Order Asc
+    file_prefix = 'Test_Order_Asc'
+    pdf_path = '../data/test-input.pdf'
+    counter = 2000
+    app(file_prefix, pdf_path, counter)
+
 
 # TODO: Add cover page functionality
-# TODO: Add outout counter functionality
-# Count by, year, year-month, month, ID
-# Consider adding a JSON map for the ID to be mapped to a category
--- src/app/test.py
+++ src/app/test.py
@@ -1,3 +1,5 @@
+import json
+
 list = [1, 0, 0, 0, 1, 0, 1]
 
 new_list = []
@@ -10,3 +12,25 @@ for item in list[1:len(list)]:
 
 path = '../out/temp/flag.pdf'
 print(path[:-4])
+
+
+# Python program to illustrate
+# enumerate function in loops
+l1 = ["eat", "sleep", "repeat"]
+
+# changing index and printing separately
+for count, file in enumerate(l1, 1):
+    print(count, file)
+
+
+def load_map(path_to_map):
+    with open(path_to_map) as json_file:
+        json_content = json.load(json_file)
+        return json_content
+
+
+map = load_map('../data/map.json')
+
+for i in range(1, 5):
+    # value =
+    print(map["{}".format(i)])
